0e5381f49b0a7f4373ce1fc65c78e2b0f9763521
GabrielTenZig <jesusgabrieltenorioziga@gmail.com>
2023-11-12 18:01:25 -0600

    Se modificó la API URL que ahora apunta al servicio de ngrk.

--- assets/react-audio-visualize.es-69216c73-53fc8311.js
+++ assets/react-audio-visualize.es-69216c73-53fc8311.js
@@ -1,4 +1,4 @@
-import{r as p,T as _}from"./index-5f9121be.js";var v={exports:{}},R={};/**
+import{r as p,T as _}from"./index-aa5bde68.js";var v={exports:{}},R={};/**
  * @license React
  * react-jsx-runtime.production.min.js
  *
